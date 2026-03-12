2c1e9d75120555c484e2209fee3ced4e6c1338f4
pipu-kun <SideActivity@proton.me>
2026-03-12 03:33:04 +0000

    Add input condition

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -5,5 +5,14 @@ int arr[20];
 int n;
 
 void input() {
-    
+        while (true) {
+        cout << "Masukan jumlah data array: ";
+        cin >> n;
+        
+        if (n <= 20) {
+            break;
+        } else {
+            cout << "\nArray tidak boleh lebih dari 20\n" << endl;
+        }
+    }
 }
